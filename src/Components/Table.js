a58e1b14d07d0135e75ca3182b2caaacd602a6f0
perumalm416 <perumalm416@gmail.com>
2023-07-26 11:10:12 +0530

    setup-ag-grid

---
+++ src/Components/Table.js
@@ -0,0 +1,17 @@
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import { Component, Fragment } from 'react';
+
+
+
+export class TableData extends Component{
+    constructor(){
+        super();
+        this.state={}
+    }
+    render(){
+        return<Fragment>
+            
+        </Fragment>
+    }
+}
